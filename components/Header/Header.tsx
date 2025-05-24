c1650ff198b1106f78372643736cdc945284719d
Dipongkor-Roy <juniordipu@gmail.com>
2025-05-25 02:57:36 +0600

    header updated

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,26 +1,23 @@
 'use client';
 
-
 import Link from 'next/link';
 import { SpinningText } from '../magicui/spinning-text';
 
 const Header: React.FC = () => {
-
-
   return (
-    <header className="w-full py-4 mb-5 px-6 md:px-8 flex justify-between items-center">
-      <div className="flex items-center">
-        <Link href="/">
-          <h1 className="text-2xl font-bold bg-gradient-to-r from-[#6366f1] to-[#a21caf] bg-clip-text text-transparent tracking-tight">
-             Noto
-          </h1>
+    <header className="w-full py-5 mb-6 px-5 flex  items-center justify-between">
+      <Link href="/">
+        <h1 className="text-2xl  font-bold bg-gradient-to-r from-[#6366f1] to-[#a21caf] bg-clip-text text-transparent tracking-tight mb-2">
+          Noto
+        </h1>
+      </Link>
+      <div className="  ">
+        <Link href="https://dipongkor-roy.vercel.app">
+          <SpinningText className="text-base mr-7 flex items-center whitespace-nowrap">
+            learn more • grow more •
+          </SpinningText>
         </Link>
       </div>
-      <Link href="https://dipongkor-roy.vercel.app" className=""> {/* Match the logo height */}
-        <SpinningText className="" /* adjust text-base/h-8 as needed */>
-          learn more • grow more •
-        </SpinningText>
-      </Link>
     </header>
   );
 };
